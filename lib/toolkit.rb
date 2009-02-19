da10745b048604821a135637ae685fe0a60729ff
Jacob Basham <jacob@killswitchcollective.com>
2009-02-18 21:06:57 -0600

    Some <strike>rants</strike> notes.

--- lib/toolkit.rb
+++ lib/toolkit.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'activesupport'
+
 begin
   require 'lorem'
 rescue
@@ -14,7 +17,7 @@ module ToolkitHelper
     }
   end
 
-  # Just your lil-olde content type
+  # Just your lil-olde content type... HAML style.
   def content_type()
     return {
       'http-equiv' => 'Content-Type',
@@ -67,17 +70,26 @@ module ToolkitHelper
     @page_title ||= "Hello World"
     return @page_title
   end
-  #memoize :page_title
+  memoize :page_title
 
   # Creates and stores list of javascripts to be included to HTML pages.
+  # Views may add scripts just as if this were an array itself.
+  #
+  # <%= javascripts << "one_line_css_converter.js" %>
+  # <%= javascripts.reject! { |r| r == "something_incompatable*.js" }
+  #
+  # TODO Should this be read only? For additional scripts to be included
+  # by the application.js, so they are only loaded when and if they are 
+  # needed?
   def javascripts
     @javascripts ||= %w{jquery.min.js jquery-ui.min.js application.js}
     return @javascripts
   end
-  #memoize :javascripts
+  memoize :javascripts
 
   # HTML to include javascripts
-  # *BE REAL* and use at the _bottom_ of <body>
+  # *BE REAL* and use at the _bottom_ of <body>!
+  # TODO don't do crazy development caching techniques on jquery.
   def include_javascript
     return javascript_include_tag javascripts()
   end
@@ -87,19 +99,28 @@ module ToolkitHelper
     @stylesheets ||= %w{layout theme}
     return @stylesheets
   end
-  #memoize :stylesheets
+  memoize :stylesheets
 
   # HTML to include the stylesheets
+  # For now we're loading blueprint via hardcoded tags in the template,
+  # which is a complete 540 from what we're doing with javascripts.
+  # It sure would save us from dealing with caching if we hardcoded the
+  # scripts. On the other hand, we'll always want blueprint around, but
+  # we might not always want to stick with jQuery.
+  # TODO iff we include blueprint here, we shouldn't cache in dev env
   def link_stylesheets
     return stylesheet_link_tag stylesheets()
   end
 
   # Creates and stores list of metatags to be included to HTML pages.
+  # I insert the content-type in the html first thing, and include
+  # the meta tags later. You might want to add it as a default here
+  # if aren't going to do that.
   def meta_tags
     @meta_tags ||= {}
     return @meta_tags
   end
-  #memoize :meta_tags
+  memoize :meta_tags
 
   # This feels a little silly, creating meta tags with ActionPack
   # here and using content_type() for HAMLs header.
@@ -120,7 +141,9 @@ module ToolkitHelper
     return image_tag('nil.gif', options)
   end
 
-  # Generates a lorem ipsum string
+  # Generates a lorem ipsum string. If you don't have the lorem gem,
+  # nothing will die, and you'll just get an empty string.
+  # This and rand_time_ago_in_words are mostly for ease in mockups.
   # length:: number of words to generate
   def jibberish(length = 50)
     return Lorem::Base.new('words', length).output
@@ -128,7 +151,7 @@ module ToolkitHelper
     return ""
   end
 
-  # Returns time_ago_in_words for a random time
+  # Returns time_ago_in_words for a random time.
   # max_minutes:: maximum number of minutes back allowed
   def rand_time_ago_in_words(max_minutes = 640)
     time_ago_in_words(Time.now - rand(max_minutes) << " ago"
